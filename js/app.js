4b7ddd2f80312c228bc39257a47cddd806b78ad7
Gabriel Santos Pole <gabriel_pole@hotmail.com>
2024-07-29 18:23:07 -0300

    Modal

--- js/app.js
+++ js/app.js
@@ -1,38 +1,38 @@
- //MODAL DE POST 
- const modal =  document.querySelector("dialog");
- const button_close = document.querySelector("dialog button");
- const button = document.querySelectorAll("div.info_modal").forEach(function(open){
-   open.addEventListener("click", function() {
-     modal.showModal();
-   });
- });
- button_close.onclick = function(){
-   modal.close()
- }
+// Portifólio Vue
+const vm = new Vue({
+    el: "#posts",
+    data: {
+       posts: {},
+       post: true
+    },
+    methods: {
+      puxarCursos() {
+        fetch("https://gabrielpole.github.io/portifolio/api/posts.json")
+         .then(r => r.json())
+         .then(r => {
+           this.posts = r;
+           console.log(r)
+        })
+      },
+      fetchPost(id){
+       fetch(`https://gabrielpole.github.io/portifolio/api/posts/post${id}/post.json`)
+         .then(r => r.json())
+         .then(r =>{
+           this.post = r
+         })
+      } 
+    },
+    created(){
+      this.puxarCursos()
+      this.fetchPost(1)
+    }
+  });
+
  //Ver mais experiência
  document.querySelectorAll(".ver_mais").forEach(function(botao) {
    botao.addEventListener("click", function() {
      this.parentElement.classList.add("quero_ver");
    });
  });
- // Portifólio Vue
- const vm = new Vue({
-     el: "#posts",
-     data: {
-       posts: {}
-     },
-     methods: {
-       puxarCursos() {
-         fetch("https://gabrielpole.github.io/portifolio/api/posts.json")
-         .then(r => r.json())
-         .then(r => {
-           this.posts = r;
-           console.log(r)
-           
-         })
-       }
-     },
-     created(){
-       this.puxarCursos()
-     }
-   });
+
+ 
